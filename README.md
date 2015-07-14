79230efe79664630520b7b862acfa9167e8ce8d2
Alan Evans <thealanevans@googlemail.com>
2015-07-14 21:18:38 +0100

    Help text

--- README.md
+++ README.md
@@ -5,15 +5,19 @@ Trouble with Android's Jobb tool? Or just need more control over compression.
 ```
 Good Jobb -- Create OBB files for use on Android
 
+Supported Jobb arguments:
  -d <directory> Use <directory> as input for OBB files
  -o <filename>  Write OBB file out to <filename>
  -o <directory> Write OBB file out to <directory>
- -res <resFile> Write a handy resource.xml file to <resFile>
  -v             Verbose mode
  -h             Help; this usage screen
  -pn <package>  Package name for OBB file
  -pv <version>  Package version for OBB file
+
+Additional arguments:
  -patch         Is patch not main
+ -res <resFile> Write a handy resource.xml file to <resFile>
+ -rp <prefix>   In the -res file, prefix each string value with this <prefix>
 
 0% compression options (repeatable)
  -0e <extension> Do not compress this extension. e.g. -0e .abc
--- src/main/java/com/coltsoftware/jobb/Main.java
+++ src/main/java/com/coltsoftware/jobb/Main.java
@@ -80,19 +80,20 @@ public class Main {
     private static void printHelp() {
         out.println("Good Jobb -- Create OBB files for use on Android\n" +
                 "\n" +
+                "Supported Jobb arguments:\n" +
                 " -d <directory> Use <directory> as input for OBB files\n" +
                 " -o <filename>  Write OBB file out to <filename>\n" +
                 " -o <directory> Write OBB file out to <directory>\n" +
-                " -res <resFile> Write a handy resource.xml file to <resFile>\n" +
-                " -rp <prefix>   In the -res file, prefix each string value with this <prefix>\n" +
                 " -v             Verbose mode\n" +
                 " -h             Help; this usage screen\n" +
                 " -pn <package>  Package name for OBB file\n" +
-                " -pv <version>  Package version for OBB file\n" +
-                " -patch         Is patch not main");
-        out.println();
-        out.println("0% compression options (repeatable)");
-        out.println(" -0e <extension> Do not compress this extension. e.g. -0e .abc");
-        out.println(" -0r <regex>     Do not compress files matching this regex. e.g. -0r .*\\\\file.abc");
+                " -pv <version>  Package version for OBB file\n");
+        out.println("Additional arguments:\n" +
+                " -patch         Is patch not main\n" +
+                " -res <resFile> Write a handy resource.xml file to <resFile>\n" +
+                " -rp <prefix>   In the -res file, prefix each string value with this <prefix>\n");
+        out.println("0% compression options (repeatable)\n" +
+                " -0e <extension> Do not compress this extension. e.g. -0e .abc\n" +
+                " -0r <regex>     Do not compress files matching this regex. e.g. -0r .*\\\\file.abc");
     }
 }
