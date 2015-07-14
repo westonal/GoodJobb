36f489fc8ad6153ea753792b2ab2f0d9e210c7ee
Alan Evans <thealanevans@googlemail.com>
2015-07-14 20:58:34 +0100

    Wrap each forwarded arg in quotes

--- GoodJobbWin32Exe/src/GoodJobb.c
+++ GoodJobbWin32Exe/src/GoodJobb.c
@@ -15,30 +15,45 @@
 int countRequired(int argc, char *argv[]) {
 	int i;
 	int c = 0;
-	for (i = 1; i < argc; i++) {
-		c += strlen(argv[i]) + 1;
+	for (i = 0; i < argc; i++) {
+		c += strlen(argv[i]) + 3;
 	}
 	return c;
 }
 
-char *buildCommandLine(char *prefix, int argc, char *argv[]) {
+char *buildCommandLine(int argc, char *argv[]) {
 	int i;
-	int required = strlen(prefix) + countRequired(argc, argv);
-	char *result = malloc(required + 1);
+	int required = countRequired(argc, argv);
+	char *result = malloc(required + 3);
 	result[0] = '\0';
-	strcat(result, prefix);
-	for (i = 1; i < argc; i++) {
-		strcat(result, " ");
+	strcat(result, "\"");
+	for (i = 0; i < argc; i++) {
+		strcat(result, "\"");
 		strcat(result, argv[i]);
+		strcat(result, "\" ");
 	}
+	result[strlen(result) - 1] = '\"';
 	return result;
 }
 
+#define arrayLength(array) (sizeof((array))/sizeof((array)[0]))
+
 int main(int argc, char *argv[]) {
-	char *prefix =
-			"\"\"%JAVA_HOME%\\bin\\java\"\" -jar %ChocolateyInstall%\\lib\\goodjobb\\tools\\goodjobb.jar";
-	char *commandLine = buildCommandLine(prefix, argc, argv);
-	system(commandLine);
-	free(commandLine);
+	int i;
+	char *prefixes[] = { "%JAVA_HOME%\\bin\\java.exe", "-jar",
+			"%ChocolateyInstall%\\lib\\goodjobb\\tools\\goodjobb.jar" };
+	int len = arrayLength(prefixes) + argc - 1;
+	char *p2[len];
+	for (i = 0; i < arrayLength(prefixes); i++) {
+		p2[i] = prefixes[i];
+	}
+	for (i = 1; i < argc; i++) {
+		p2[i + arrayLength(prefixes) - 1] = argv[i];
+	}
+	{
+		char *commandLine = buildCommandLine(len, p2);
+		system(commandLine);
+		free(commandLine);
+	}
 	return EXIT_SUCCESS;
 }
--- tools/chocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-Generate-BinFile -name "GoodJobb" -path "$env:JAVA_HOME\bin\java.exe" -command "`"-jar $env:ChocolateyInstall\lib\GoodJobb\tools\GoodJobb.jar`""
