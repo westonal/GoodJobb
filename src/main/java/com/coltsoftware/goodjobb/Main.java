2fd70173022231e37175d9b582cf16bc95b80f58
Alan Evans <thealanevans@googlemail.com>
2015-07-15 21:20:51 +0100

    Target Java 1.7

--- src/main/java/com/coltsoftware/goodjobb/Main.java
+++ src/main/java/com/coltsoftware/goodjobb/Main.java
@@ -34,14 +34,17 @@ public class Main {
 
         Zipper zipper = new Zipper(new File(pathname), outputName);
 
-        args.list("-0e").forEach(zipper::addNoCompressExtension);
-        args.list("-0r").forEach(zipper::addNoCompressRegex);
+        for (String extension : args.list("-0e"))
+            zipper.addNoCompressExtension(extension);
+        for (String regex : args.list("-0r"))
+            zipper.addNoCompressRegex(regex);
 
         if (verbose) {
             out.println("Creating obb zip: " + outputName);
             if (zipper.getNoCompressPatterns().size() > 0) {
                 out.println("These patterns will not be compressed");
-                zipper.getNoCompressPatterns().forEach(out::println);
+                for (CompressionPattern pattern : zipper.getNoCompressPatterns())
+                    out.println(pattern);
             }
         }
 
@@ -49,7 +52,8 @@ public class Main {
 
         if (verbose) {
             out.println("Files:");
-            zipResult.getAddedFiles().forEach(out::println);
+            for (Zipper.ZipResult.ZipEntryDetails file : zipResult.getAddedFiles())
+                out.println(file);
         }
 
         out.println("Complete");
